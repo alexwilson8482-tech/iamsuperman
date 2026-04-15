37a6780ed905ed920795583aa2970426b0bc1819
alexwilson8482-tech <alexwilson8482@gmail.com>
2026-04-15 13:36:38 +0530

    Update BundlesPage.tsx

--- src/pages/BundlesPage.tsx
+++ src/pages/BundlesPage.tsx
@@ -5,17 +5,6 @@ interface BundlesPageProps {
   apis: ApiPanel[];
   bundles: Bundle[];
   onAddBundle: (bundle: {
-  name: string;
-  apiId: string;
-  views: string;
-  likes: string;
-  shares: string;
-  saves: string;
-  comments: string;
-}) => void;
-  onUpdateBundle: (
-  id: string,
-  bundle: {
     name: string;
     apiId: string;
     views: string;
@@ -23,8 +12,19 @@ interface BundlesPageProps {
     shares: string;
     saves: string;
     comments: string;
-  }
-) => void;
+  }) => void;
+  onUpdateBundle: (
+    id: string,
+    bundle: {
+      name: string;
+      apiId: string;
+      views: string;
+      likes: string;
+      shares: string;
+      saves: string;
+      comments: string;
+    }
+  ) => void;
   onDeleteBundle: (id: string) => void;
 }
 
